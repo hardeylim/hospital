055beee0025b788a77dd16c025ee88faee990fba
Hardey <hardeylim@gmail.com>
2020-03-09 23:48:05 +0800

    Update nurse controller to create nurse with user

--- app/controllers/api/nurses_controller.rb
+++ app/controllers/api/nurses_controller.rb
@@ -6,11 +6,11 @@ class Api::NursesController < ApplicationController
     @nurses = []
     Nurse.order('id DESC').each do |nurse|
       @nurses.push({
-                 id: nurse.id,
-                 name: nurse.user.name,
-                 patients: nurse.patients,
-                 head_nurse: nurse.head_nurse
-               })
+                       id: nurse.id,
+                       name: nurse.user.name,
+                       patients: nurse.patients,
+                       head_nurse: nurse.head_nurse
+                   })
     end
     render json: @nurses
   end
@@ -21,9 +21,28 @@ class Api::NursesController < ApplicationController
     render json: @nurse
   end
 
-  def update
+  def create
+    if (@user = User.create(nurse_params))
+      @nurse = Nurse.create(user_id: @user.id)
+    end
 
+    render json: @nurse
   end
 
+  def edit
+  end
 
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def nurse_params
+    params.require(:nurse).permit(
+        :name
+    )
+  end
 end
