ccd92c41895ff3ed3c2cb65d80bac5c528dd5a72
Hardey <hardeylim@gmail.com>
2020-03-11 00:22:00 +0800

    Add new head nurse builder

---
+++ app/services/head_nurses/builder.rb
@@ -0,0 +1,25 @@
+class HeadNurses::Builder
+  def index
+    # @nurses = Nurse.all
+    @head_nurses = []
+    # includes prevent N+1
+    # rule of thumb. dapat walang ganitong shit sa controller
+    # dapat controller simpol lng boi
+    #
+    HeadNurse.order('id DESC').each do |head_nurse|
+      @head_nurses.push({
+                           id: head_nurse.id,
+                           name: head_nurse.nurse.user.name
+                       })
+    end
+    # Nurse.includes(:patients).order('id DESC').each do |nurse|
+    #   @nurses.push({
+    #                  id: nurse.id,
+    #                  name: nurse.user.name,
+    #                  patients: Patients::Builder.new.format_patients(nurse.patients),
+    #                  head_nurse: nurse.head_nurse
+    #                })
+    # end
+    @head_nurses
+  end
+end
