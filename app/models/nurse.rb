6032e77d5fe9bab7d4caf0b2273e40c06ed5a671
Hardey <hardeylim@gmail.com>
2020-03-11 00:21:50 +0800

    Add relationship nurse to head nurse

--- app/models/nurse.rb
+++ app/models/nurse.rb
@@ -2,5 +2,5 @@ class Nurse < ApplicationRecord
   belongs_to :user
   has_many :nurse_patients, dependent: :destroy
   has_many :patients, through: :nurse_patients
-  has_one :head_nurse
+  has_one :head_nurse, dependent: :destroy
 end
