51f12b90614976fbd9f30d11d209af1ceacad655
Hardey Lim <hardeylim@gmail.com>
2020-03-13 20:21:11 +0800

    Add chat rooms controller, update chat room custom through

--- app/controllers/api/chat_rooms_controller.rb
+++ app/controllers/api/chat_rooms_controller.rb
@@ -1,5 +1,24 @@
 
 class Api::ChatRoomsController < ApplicationController
+
   def index
+    render json: ChatRoom.order('id DESC')
   end
+
+  def create
+    ChatRoom.create(name: params[:name])
+  end
+
+
+
+
+
+  # GET chat_rooms
+  # GET chat_rooms/:id
+  # POST chat_rooms
+  # DELTE chat_rooms/:id
+  # PATCH chat_rooms/:id
+
+  # POST chat_rooms/:id/messages
+  # DELETE chat_rooms/:id/messages/:id
 end
--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -1,12 +1,17 @@
 class ChatRoom < ApplicationRecord
-  belongs_to 'Creator', class_name: 'User', foreign_key: 'creator_id'
+  belongs_to 'creator', class_name: 'User', foreign_key: 'creator_id'
 
   has_many :chat_room_users
   # inulit
   # has_many :users, through: :chat_room_users
+  #
+  has_many :active_users, class_name: 'User', foreign_key: 'user_id', through: :chat_room_users
+
+  # ChatRoom.find(1).active_users()
 
   has_many :chat_messages
   has_many :users, through: :chat_messages
 
+  # ChatRoom.find(1).users
   validates :name, presence: true
 end
